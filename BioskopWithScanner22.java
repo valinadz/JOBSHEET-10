490c167c66c5862e71d1d929e6e0310a743eab4d
valinadz <valinaadzra@gmail.com>
2023-11-02 09:30:25 +0700

    2.2

--- BioskopWithScanner22.java
+++ BioskopWithScanner22.java
@@ -9,23 +9,54 @@ public class BioskopWithScanner22 {
         String[][] penonton = new String[4][2];
 
         while (true) {
-            System.out.print("Masukkan nama: ");
-            nama = sc.nextLine();
-            System.out.print("Masukkan Baris: ");
-            baris = sc.nextInt();
-            System.out.print("Masukkan kolom: ");
-            kolom = sc.nextInt();
+            System.out.println("Menu: ");
+            System.out.println("1. Input Data Penonton");
+            System.out.println("2. Tampilkan Daftar Penonton");
+            System.out.println("3. Exit");
+            System.out.println("Pilih Menu (1/2/3): ");
+            int menu = sc.nextInt();
             sc.nextLine();
 
-            penonton[baris-1][kolom-1] = nama;
+            switch (menu) {
+                case 1:
+                    System.out.print("Masukkan nama: ");
+                    nama = sc.nextLine();
+                    System.out.print("Masukkan Baris: ");
+                    baris = sc.nextInt();
+                    System.out.print("Masukkan kolom: ");
+                    kolom = sc.nextInt();
+                    sc.nextLine();
 
-            System.out.print("Input penonton lainnya? (y/n): ");
-            next = sc.nextLine();
+                    penonton[baris-1][kolom-1] = nama;
 
-            if (next.equalsIgnoreCase("n")) {
-                break;
+                    System.out.println("Input penonton lainnya? (y/n): ");
+                    next = sc.nextLine();
+
+                    if (next.equalsIgnoreCase("n")) {
+                        break;
+                    }
+                    break;
+
+                case 2:
+                    System.out.println("Daftar Penonton: ");
+                    for (int i = 0; i < 4; i++) {
+                        for (int j = 0; j < 2; j++) {
+                            if (penonton[i][j] != null) {
+                                System.out.println("Baris " + (i+1) + ", Kolom " + (j+1) + ": " + penonton[i][j]);
+                            }
+                        }
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("Terimakasih Telah Berunjung!");
+                    System.exit(0);
+                    break;
+                
+                default:
+                    System.out.println("Menu tidak valid.");
+                    break;
             }
-
         }
     }
 }
