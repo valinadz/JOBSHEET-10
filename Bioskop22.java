909e4bf29c73794288801d816472b476a9fb7156
valinadz <valinaadzra@gmail.com>
2023-11-02 08:43:32 +0700

    Nomor 10

--- Bioskop22.java
+++ Bioskop22.java
@@ -12,10 +12,10 @@ public class Bioskop22 {
         penonton[3][0] = "Gisel";
         penonton[3][1] = "Hana";
 
-        System.out.println(penonton.length);
+        System.out.println("Penonton pada baris ke-3: ");
         
-        for (String[] barisPenonton : penonton) {
-            System.out.println("Panjang baris: " + barisPenonton.length);
+        for (int i = 0; i < penonton[2].length; i++) {
+            System.out.println(penonton[2][i]);
         }
     }
 }
